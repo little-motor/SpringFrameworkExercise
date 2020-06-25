f0335f45b0a7619a86359d48369c3417db6312d6
Little Motor <479217391@qq.com>
2020-06-25 20:25:05 +0800

    v1 版本基础框架已经建好了

--- src/main/java/cn/littlemotor/mvcframework/service/impl/ServiceImpl.java
+++ src/main/java/cn/littlemotor/mvcframework/service/impl/ServiceImpl.java
@@ -1,8 +1,10 @@
 package cn.littlemotor.mvcframework.service.impl;
 
+import cn.littlemotor.mvcframework.annotation.Service;
 import cn.littlemotor.mvcframework.service.BaseService;
 
-public class Service implements BaseService {
+@Service
+public class ServiceImpl implements BaseService {
   @Override
   public String get(String name) {
     return "name is " + name;
--- src/main/java/cn/littlemotor/mvcframework/servlet/DispatcherServlet.java
+++ src/main/java/cn/littlemotor/mvcframework/servlet/DispatcherServlet.java
@@ -1,5 +1,10 @@
 package cn.littlemotor.mvcframework.servlet;
 
+import cn.littlemotor.mvcframework.annotation.Autowired;
+import cn.littlemotor.mvcframework.annotation.Controller;
+import cn.littlemotor.mvcframework.annotation.RequestMapping;
+import cn.littlemotor.mvcframework.annotation.Service;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,12 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Properties;
+import java.util.*;
 
 public class DispatcherServlet extends HttpServlet {
 
@@ -33,12 +36,13 @@ public class DispatcherServlet extends HttpServlet {
     try {
       doDispatcher(req, resp);
     } catch (Exception e) {
+      e.printStackTrace();
       resp.getWriter().write("500 Exception " + e.getMessage());
     }
   }
 
   private void doDispatcher(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-    String url = req.getRequestURL().toString();
+    String url = req.getRequestURI();
     String contextPath = req.getContextPath();
     url = url.replace(contextPath, "").replaceAll("/+", "/");
     if (!this.mapping.containsKey(url)){
@@ -47,7 +51,8 @@ public class DispatcherServlet extends HttpServlet {
     }
     Method method = (Method) mapping.get(url);
     Map<String, String[]> params = req.getParameterMap();
-    method.invoke(this.mapping.get(method.getDeclaringClass().getName()), new Object[]{req, resp, params.get("name")[0]});
+    Object object = this.mapping.get(method.getDeclaringClass().getName());
+    method.invoke(object, req, resp, params.get("name")[0]);
   }
 
   @Override
@@ -60,14 +65,64 @@ public class DispatcherServlet extends HttpServlet {
       configContext.load(inputStream);
       String scanPackage = configContext.getProperty("scanPackage");
       doScanner(scanPackage);
-      Thread.currentThread();
+      //注意此处只所以新建set是因为后面mapping会有新增元素不能直接对mapping.keyset遍历
+      Set<String> keys = new HashSet<>(mapping.keySet());
+      for (String className : keys) {
+        if (!className.contains(".")) return;
+        Class<?> clazz = Class.forName(className);
+        //注册Controller
+        if (clazz.isAnnotationPresent(Controller.class)) {
+          mapping.put(className, clazz.newInstance());
+          String baseUrl = "";
+          if (clazz.isAnnotationPresent(RequestMapping.class)) {
+            baseUrl = clazz.getAnnotation(RequestMapping.class).value();
+          }
+          Method[] methods = clazz.getMethods();
+          for (Method method : methods) {
+            if (!method.isAnnotationPresent(RequestMapping.class)) continue;
+            String url = (baseUrl + method.getAnnotation(RequestMapping.class).value()).replaceAll("/+", "/");
+            mapping.put(url, method);
+            System.out.println("Mapped " + url + "," + method);
+          }
+        }
+        //注册Service
+        else if (clazz.isAnnotationPresent(Service.class)) {
+          String beanName = clazz.getAnnotation(Service.class).value();
+          if (beanName.equals("")) {
+            beanName = clazz.getName();
+          }
+          Object instance = clazz.newInstance();
+          mapping.put(beanName, instance);
+          for (Class<?> interfaze : clazz.getInterfaces()) {
+            mapping.put(interfaze.getName(), instance);
+          }
+        }
+      }
+      //给mapping中的value对象注入@autowire
+      for (Object object : mapping.values()) {
+        if(Objects.isNull(object)) continue;
+        Class clazz = object.getClass();
+        if (clazz.isAnnotationPresent(Controller.class)) {
+          Field[] fields = clazz.getDeclaredFields();
+          for (Field field : fields) {
+            if (field.isAnnotationPresent(Autowired.class)) {
+              Autowired autowired = field.getAnnotation(Autowired.class);
+              String beanName = autowired.value();
+              if ("".equals(beanName)) {
+                beanName = field.getType().getName();
+              }
+              field.setAccessible(true);
+              try {
+                field.set(object, mapping.get(beanName));
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+            }
+          }
+        }
 
-
-
-
-
+      }
     } catch (Exception e) {
-
     } finally {
       if (Objects.nonNull(inputStream)) {
         try {
@@ -77,6 +132,7 @@ public class DispatcherServlet extends HttpServlet {
         }
       }
     }
+    System.out.println("MVC framework has inited");
   }
 
   private void doScanner(String packageName) {
